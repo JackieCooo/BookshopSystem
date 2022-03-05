316c55c72bfb1e0be52d5f7221c57333094bf4cb
JackieCooo <1131390120@qq.com>
2022-03-05 22:12:53 +0800

    first commit

---
+++ src/components/BriefChartBoard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <el-image :src="src" fit="cover"></el-image>
+    <h1 class="chart-title">{{title}}</h1>
+    <p class="subtitle">查看更多 ></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BriefChartBoard",
+  props: {
+    title: String,
+    src: String,
+  }
+}
+</script>
+
+<style scoped>
+.el-image {
+  height: 250px;
+  width: 250px;
+  border-radius: 10px;
+  filter: brightness(30%);
+}
+
+.chart-title {
+  position: absolute;
+  top: 35px;
+  width: 100%;
+  text-align: center;
+  font-size: 50px;
+  color: white;
+}
+
+.subtitle {
+  position: absolute;
+  bottom: 10px;
+  width: 100%;
+  text-align: center;
+  color: white;
+  opacity: 0;
+  transition: .5s ease;
+}
+
+.container:hover .subtitle{
+  opacity: 1;
+}
+</style>
---
+++ src/components/ChartPage.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <el-space direction="vertical" size="large">
+      <DetailedChartBoard :info="hottestSeller"></DetailedChartBoard>
+      <DetailedChartBoard :info="newestSeller"></DetailedChartBoard>
+      <el-space size="large">
+        <BriefChartBoard :title="literatureChart.title" :src="literatureChart.src"></BriefChartBoard>
+        <BriefChartBoard :title="historyChart.title" :src="historyChart.src"></BriefChartBoard>
+        <BriefChartBoard :title="societyChart.title" :src="societyChart.src"></BriefChartBoard>
+      </el-space>
+      <el-space size="large">
+        <BriefChartBoard :title="novelChart.title" :src="novelChart.src"></BriefChartBoard>
+        <BriefChartBoard :title="scienceChart.title" :src="scienceChart.src"></BriefChartBoard>
+        <BriefChartBoard :title="artChart.title" :src="artChart.src"></BriefChartBoard>
+      </el-space>
+    </el-space>
+  </div>
+</template>
+
+<script>
+import DetailedChartBoard from "@/components/DetailedChartBoard";
+import BriefChartBoard from "@/components/BriefChartBoard";
+
+export default {
+  name: "ChartPage",
+  components: {
+    DetailedChartBoard,
+    BriefChartBoard,
+  },
+  data() {
+    return {
+      hottestSeller: {
+        title: '畅销榜',
+        firstBookInfo: {
+          id: 1,
+          lastRank: 2,
+          name: '从零开始的女性主义',
+          author: '[日]上野千鹤子/[日]田房永子',
+          picSrc: require('@/assets/bookChart/bestSeller.jpg'),
+          time: '2021-9',
+          publisher: '北京联合出版公司',
+          price: 52.00,
+          hasEBook: true
+        },
+        bookInfo: [
+          {id: 2, lastRank: 1, name: '置身事内：中国政府与经济发展', author: '兰小欢'},
+          {id: 3, lastRank: 3, name: '也许你该找个人聊聊', author: '[美]洛莉·戈特利布'},
+          {id: 4, lastRank: -1, name: '克拉拉与太阳', author: '[英]石黑一雄'},
+          {id: 5, lastRank: -1, name: '工作、消费主义和新穷人', author: '[英]齐格蒙特·鲍曼'}
+        ]
+      },
+      newestSeller: {
+        title: '新书榜',
+        firstBookInfo: {
+          id: 1,
+          lastRank: 2,
+          name: '审美教育书简',
+          author: '[德] 弗里德里希•席勒',
+          picSrc: require('@/assets/bookChart/newestSeller.jpg'),
+          time: '2022-1',
+          publisher: '上海人民出版社',
+          price: 56.00,
+          hasEBook: true
+        },
+        bookInfo: [
+          {id: 2, lastRank: 1, name: '民族的神话：欧洲的中世纪起源', author: '[美]帕特里克·格里'},
+          {id: 3, lastRank: 3, name: '乐园之丘：权力诞生与被剥夺的历史', author: '[荷]米尼克•希珀'},
+          {id: 4, lastRank: -1, name: '崩溃：社会如何选择成败兴亡', author: '[美]贾雷德·戴蒙德'},
+          {id: 5, lastRank: -1, name: '妇女、家庭与法律实践：清代以来的法律社会史', author: '赵刘洋'}
+        ]
+      },
+      literatureChart: {
+        title: '文学榜',
+        src: require('@/assets/bookChart/literatureCover.jpg'),
+      },
+      novelChart: {
+        title: '小说榜',
+        src: require('@/assets/bookChart/novelCover.jpg'),
+      },
+      historyChart: {
+        title: '历史榜',
+        src: require('@/assets/bookChart/historyCover.jpg'),
+      },
+      societyChart: {
+        title: '文学榜',
+        src: require('@/assets/bookChart/societyCover.jpg'),
+      },
+      scienceChart: {
+        title: '文学榜',
+        src: require('@/assets/bookChart/scienceCover.jpg'),
+      },
+      artChart: {
+        title: '文学榜',
+        src: require('@/assets/bookChart/artCover.jpg'),
+      },
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
